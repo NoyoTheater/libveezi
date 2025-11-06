8f1eaffa42f32a3e0f929799005b93ffb34a438e
0xLogN <nutdriver716@gmail.com>
2025-11-06 13:17:32 -0800

    feat: impl `Error` and `Display` for `LibVeeziError`

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,31 @@ use chrono::NaiveDateTime;
 use log::debug;
 use reqwest::Url;
 use serde::{Deserialize, de::DeserializeOwned};
-use std::fmt::Debug;
+use std::fmt::{self, Debug};
 
 /// The list of errors that can occur when using the libveezi library
+#[derive(Debug)]
 pub enum LibVeeziError {
     /// An error occurred while making an HTTP request
     Http(reqwest::Error),
     /// An error occurred while parsing a URL
     UrlParse(url::ParseError),
+}
+impl fmt::Display for LibVeeziError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            LibVeeziError::Http(err) => write!(f, "HTTP error: {}", err),
+            LibVeeziError::UrlParse(err) => write!(f, "URL parse error: {}", err),
+        }
+    }
+}
+impl std::error::Error for LibVeeziError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            LibVeeziError::Http(err) => Some(err),
+            LibVeeziError::UrlParse(err) => Some(err),
+        }
+    }
 }
 impl From<reqwest::Error> for LibVeeziError {
     fn from(err: reqwest::Error) -> Self {
